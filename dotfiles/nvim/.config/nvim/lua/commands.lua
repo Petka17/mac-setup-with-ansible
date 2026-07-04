c3ea64f1f46a871b9722817adbe50f9a9b8513fb
Peter K <mail+github@petka.dev>
2026-07-04 08:33:41 +0200

    Cleanup: sesh wildcards, tmux kill-session, nvim cursor restore, add obsidian and discord

--- dotfiles/nvim/.config/nvim/lua/commands.lua
+++ dotfiles/nvim/.config/nvim/lua/commands.lua
@@ -1,3 +1,16 @@
+-- Restore cursor to last known position when reopening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Restore cursor to last known position",
+  group = vim.api.nvim_create_augroup("restore-cursor", { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
